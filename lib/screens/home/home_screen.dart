be1c4befdb6d895d9d33ab3280348ac971436932
shimizu-saffle <shimizu.saffle@gmail.com>
2022-08-30 22:34:37 +0900

    feat: _buildAppBar() on HomeScreen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:travel/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: _buildAppBar(),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      leading: IconButton(
+        icon: Icon(
+          Icons.menu,
+          color: kIconColor,
+        ),
+        onPressed: () {},
+      ),
+      actions: [
+        IconButton(
+          icon: ClipOval(
+            child: Image.asset('assets/images/profile.png'),
+          ),
+          onPressed: () {},
+        ),
+      ],
+    );
   }
 }
