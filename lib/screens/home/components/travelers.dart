abc41965e9e56f91b99416f2821db521fe6ce33b
shimizu-saffle <shimizu.saffle@gmail.com>
2022-09-03 09:57:03 +0900

    define: Travelers

---
+++ lib/screens/home/components/travelers.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+import '../../../models/TravelSpot.dart';
+import '../../../size_config.dart';
+
+class Travelers extends StatelessWidget {
+  const Travelers({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    int totalUser = 0;
+    return SizedBox(
+      width: double.infinity,
+      height: getProportionateScreenWidth(30),
+      child: Stack(
+        children: [
+          ...List.generate(
+            travelSpots[0].users.length,
+            (index) {
+              totalUser++;
+              return Positioned(
+                left: (22 * index).toDouble(),
+                child: buildTravelerFace(index),
+              );
+            },
+          ),
+          Positioned(
+            left: (22 * totalUser).toDouble(),
+            child: SizedBox(
+              height: getProportionateScreenHeight(28),
+              width: getProportionateScreenWidth(28),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.zero,
+                  backgroundColor: kPrimaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                onPressed: () {},
+                child: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  ClipOval buildTravelerFace(int index) {
+    return ClipOval(
+      child: Image.asset(
+        travelSpots[0].users[index].image,
+        height: getProportionateScreenHeight(28),
+        width: getProportionateScreenWidth(28),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
